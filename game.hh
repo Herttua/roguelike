7828a7525ac245ab2d2c37a54ae93ba01a1648ee
Herttua <juha.salinmaki@gmail.com>
2024-09-07 16:34:38 +0300

    Added a source file for game class.

--- game.hh
+++ game.hh
@@ -1,14 +1,27 @@
 #ifndef GAME_HH_
 #define GAME_HH_
 
+#include <SDL2/SDL.h>
+
 class game
 {
 public:
     game() {}
     ~game() {}
 
-private:
+    void init(const char* title, 
+              int x, int y, 
+              int w, int h,
+              bool fullscreen);
+    void update();
+    void render();
+    void clean();
+    bool running();
 
+private:
+    bool quit;
+    SDL_Window*   window;
+    SDL_Renderer* renderer;
 };
 
 #endif //GAME_HH_
