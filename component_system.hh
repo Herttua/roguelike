d8ddbf06867a13be9a1267563bac52932e3ea736
Herttua <juha.salinmaki@gmail.com>
2024-09-09 19:40:02 +0300

    Minor changes.

--- component_system.hh
+++ component_system.hh
@@ -47,10 +47,13 @@ public:
     void update()
     {
         for(auto& c : components) c->update();
-        for(auto& c : components) c->draw();
     }
 
-    void draw() {}
+    void draw() 
+    {
+        for(auto& c : components) c->draw();
+    }
+    
     bool is_active() const { return active; }
     void destroy() { active = false; }
 
