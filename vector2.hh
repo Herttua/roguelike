0950aa4a1ca08fdd77ceaef60a8285d6237a0d49
Herttua <juha.salinmaki@gmail.com>
2024-09-11 11:02:52 +0300

    Working on the vector2 class.

--- vector2.hh
+++ vector2.hh
@@ -9,6 +9,13 @@ class vector2
     vector2() {}
     vector2(float x_pos, float y_pos) : x(x_pos), y(y_pos) {}
     ~vector2() {}
+
+    vector2& add(const vector2& vec);
+    vector2& subtract(const vector2& vec);
+    vector2& multiply(const vector2& vec);
+    vector2& divide(const vector2& vec);
+
+    friend vector2& operator+(vector2& v1, const vector2& v2); 
 };
 
 #endif //VECTOR2_HH_
